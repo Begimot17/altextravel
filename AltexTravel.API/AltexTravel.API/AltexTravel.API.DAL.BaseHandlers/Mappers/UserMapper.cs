2fd4d9fe68d5941182f3407044bcb79b5654b7fa
Begimot17 <nikitadyachkovchik@gmail.com>
2019-08-28 11:30:07 +0300

    Class Library to ASPNETCORE

--- AltexTravel.API/AltexTravel.API/AltexTravel.API.DAL.BaseHandlers/Mappers/UserMapper.cs
+++ AltexTravel.API/AltexTravel.API/AltexTravel.API.DAL.BaseHandlers/Mappers/UserMapper.cs
@@ -1,10 +1,5 @@
 ﻿using AltexTravel.API.DAL.Entity;
 using AltexTravel.API.Domain.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AltexTravel.API.DAL.BaseHandlers.Mappers
 {
--- AltexTravel.API/AltexTravel.API/AltexTravel.API.DAL/Mappers/UserMapper.cs
+++ AltexTravel.API/AltexTravel.API/AltexTravel.API.DAL/Mappers/UserMapper.cs
@@ -1,10 +1,5 @@
 ﻿using AltexTravel.API.DAL.Entity;
 using AltexTravel.API.Dtos.User;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AltexTravel.API.DAL.Mappers
 {
