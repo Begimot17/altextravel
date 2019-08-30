d0fbc5b320a400f85fd1795c0b8d84b856500e65
Begimot17 <nikitadyachkovchik@gmail.com>
2019-08-30 14:56:45 +0300

    commit

--- AltexTravel.API/AltexTravel.API/AltexTravel.API.DAL.BaseHandlers/BaseCommandHandler.cs
+++ AltexTravel.API/AltexTravel.API/AltexTravel.API.DAL.BaseHandlers/BaseCommandHandler.cs
@@ -1,32 +1,25 @@
-﻿using FluentValidation;
-using FluentValidation.Results;
-using System.Collections.Generic;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
+using AltexTravel.API.Domain;
+using FluentValidation;
+using MediatR;
 
 namespace AltexTravel.API.DAL.BaseHandlers
 {
-    public abstract class BaseCommandHandler<TRequest ,ValidateResponse<TResponse>> :  ValidateResponse<TResponse>, IRequestHandler<TRequest> 
-        {   private readonly IValidator<TRequest> _validator;
-
-    protected BaseCommandHandler(IValidator<TRequest> validator)
+    public abstract class BaseCommandHandler<TRequest, ValidateResponse<TResponse>> : IRequestHandler<TRequest, ValidateResponse<TResponse>> where TRequest : IRequest<ValidateResponse<TResponse>>
     {
-        _validator = validator;
+        private readonly IValidator<TRequest> _validator;
+
+        protected BaseCommandHandler(IValidator<TRequest> validator)
+        {
+            _validator = validator;
+        }
+        protected abstract Task<ValidateResponse<TResponse>> HandleAsync(TRequest request, CancellationToken cancellationToken);
+        public Task<ValidateResponse<TResponse>> Handle(TRequest request, CancellationToken cancellationToken)
+        {
+            var validationRezult = _validator.Validate(request);
+            return HandleAsync(request, cancellationToken);
+        }
+        
     }
-    protected abstract Task<ValidateResponse<TResponse>> HandleAsync(TRequest request, CancellationToken cancellationToken);
-    public Task<ValidateResponse<TResponse>> Handle(TRequest request, CancellationToken cancellationToken)
-    {
-        var validationRezult = _validator.Validate(request);
-        return HandleAsync(request, cancellationToken);
-    }
 }
-
-
-public class ValidateResponse<TResult>
-{
-    public virtual bool IsValid { get; }
-    public IList<ValidationFailure> Errors { get; }
-    public TResult Result { get; set; }
-}
-}
-
---
+++ AltexTravel.API/AltexTravel.API/AltexTravel.API.DAL.BaseHandlers/ValidateResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using FluentValidation.Results;
+
+public class ValidateResponse<TResult>
+{
+    public virtual bool IsValid { get; }
+    public IList<ValidationFailure> Errors { get; }
+    public TResult Result { get; set; }
+}
+}
