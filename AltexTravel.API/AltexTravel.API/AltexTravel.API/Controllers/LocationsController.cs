84b5fe43c62ae7b51e139616e7a08a59e9cc7f99
Begimot17 <nikitadyachkovchik@gmail.com>
2019-09-04 10:39:19 +0300

    Add contracts and mocks (LOCATION)

--- AltexTravel.API/AltexTravel.API/AltexTravel.API/Controllers/LocationsController.cs
+++ AltexTravel.API/AltexTravel.API/AltexTravel.API/Controllers/LocationsController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using MediatR;
-using Microsoft.AspNetCore.Http;
+﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace AltexTravel.API.Controllers
 {
--- AltexTravel.API/AltexTravel.API/AltexTravel.API/Enums/Cabin.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AltexTravel.API.Models.Flights
-{
-    enum Cabin
-    {
-        Economy=1, PremiumEconomy=2, Business=3,First=4
-    }
-}
