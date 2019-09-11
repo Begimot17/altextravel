f567c7f14a6fb9be290b998d64d3b2f7571546b9
Begimot17 <nikitadyachkovchik@gmail.com>
2019-09-11 10:30:58 +0300

    Fix

--- AltexTravel.API/AltexTravel.API/AltexTravel.API.Amadeus/AmadeusManager.cs
+++ AltexTravel.API/AltexTravel.API/AltexTravel.API.Amadeus/AmadeusManager.cs
@@ -68,7 +68,7 @@ namespace AltexTravel.API.Amadeus
             var airports = data.Data.Where(x => x.Type == LocationsEnum.AIRPORT.ToString()).ToList();
             if (airports.Count==0)
             {
-                foreach (var city in data.Data.Where(x => x.Type == LocationsEnum.CITY.ToString()))
+                foreach (var city in data.Data.Where(x => x.Type == LocationsEnum.CITY.ToString()).ToList())
                 {
                     city.Airports = new List<IataAmadeus>();
                     foreach (var air in airports)
