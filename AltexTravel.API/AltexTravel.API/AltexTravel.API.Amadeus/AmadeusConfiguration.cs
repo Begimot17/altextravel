e1b5ba99dfa7f3cf9b39ee65b999acea814ba3be
Begimot17 <nikitadyachkovchik@gmail.com>
2019-09-12 09:41:24 +0300

    Fixed

--- AltexTravel.API/AltexTravel.API/AltexTravel.API.Amadeus/AmadeusConfiguration.cs
+++ AltexTravel.API/AltexTravel.API/AltexTravel.API.Amadeus/AmadeusConfiguration.cs
@@ -7,11 +7,11 @@ namespace AltexTravel.API.Amadeus
 {
     public class AmadeusConfiguration
     {
-        public string UrlLocations = "https://test.api.amadeus.com/v1/reference-data/locations?subType=AIRPORT,CITY&keyword=r&page[limit]=5000";
+        public string UrlLocations => $"{BaseUri}reference-data/locations?subType=AIRPORT,CITY&keyword=r&page[limit]=5000";
         public string Apikey = "gZCS7tiyaRIIVihAoQFo2vARev7AnAVh";
         public string Apisecret = "yb3SXyWAeLGblN8K";
-        public string TokenURL = "https://test.api.amadeus.com/v1/security/oauth2/token";
-        public string BaseUrl = "https://test.api.amadeus.com/v1/";
+        public string TokenURL => $"{BaseUri}security/oauth2/token";
+        public string BaseUri = "https://test.api.amadeus.com/v1/";
         public string Post => $"grant_type=client_credentials&client_id={Apikey}&client_secret={Apisecret}";
     }
 }
