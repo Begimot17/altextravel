411c0929808129898805fe65388b8059ac51a309
Begimot17 <nikitadyachkovchik@gmail.com>
2019-09-30 12:11:29 +0300

    Add Cache

--- AltexTravel.API/AltexTravel.API/AltexTravel.API/Controllers/FlightsSearchController.cs
+++ AltexTravel.API/AltexTravel.API/AltexTravel.API/Controllers/FlightsSearchController.cs
@@ -1,12 +1,15 @@
 ﻿using AltexTravel.API.DAL.Queries.Features.Recommendations;
+using AltexTravel.API.DAL.QueryHandlers.Features.Recommendations;
 using AltexTravel.API.Mappers;
 using AltexTravel.API.Models.SearchResult;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Nancy.Json;
 
 namespace AltexTravel.API.Controllers
 {
@@ -15,9 +18,12 @@ namespace AltexTravel.API.Controllers
     public class FlightsSearchController : ControllerBase
     {
         private readonly IMediator _mediator;
-        public FlightsSearchController(IMediator mediator)
+        private readonly IMemoryCache _cache;
+
+        public FlightsSearchController(IMediator mediator, IMemoryCache cache)
         {
             _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            _cache = cache;
         }
 
         [HttpGet]
@@ -25,9 +31,16 @@ namespace AltexTravel.API.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> RoundTrip([FromQuery]RecommendationQuery recommendationQuery)
         {
-            var responce = await _mediator.Send(recommendationQuery);
-            var result = responce.Result?.ToViewModel();
-            return new OkObjectResult(result);
+            var json = new JavaScriptSerializer().Serialize(recommendationQuery);
+            var recommendations = _cache.Get<RecommendationsViewModel>(json);
+            if (recommendations == null)
+            {
+                var responce = await _mediator.Send(recommendationQuery);
+                recommendations = responce.Result?.ToViewModel();
+                _cache.Set(json, recommendations, new MemoryCacheEntryOptions()
+                    .SetAbsoluteExpiration(TimeSpan.FromMinutes(3)));
+            }
+            return new OkObjectResult(recommendations);
         }
     }
 }
